02da00dcb111e0153640009d32ea700ad97f9ff2
rohannagpure45 <nagpure.r@northeastern.edu>
2025-06-25 16:19:32 -0500

    kotlin compatibility changes + debugging

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,8 +18,8 @@
 
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "8.1.0" apply false
-    id("com.android.library") version "8.1.0" apply false
+    id("com.android.application") version "8.10.1" apply false
+    id("com.android.library") version "8.10.1" apply false
     id("org.jetbrains.kotlin.android") version "1.9.0" apply false
     id("com.google.devtools.ksp") version "1.8.10-1.0.9" apply false
     id("com.google.dagger.hilt.android") version "2.51" apply false
@@ -29,7 +29,7 @@ buildscript {
         google()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:8.1.0")
+        classpath("com.android.tools.build:gradle:8.10.1")
         classpath ("androidx.navigation:navigation-safe-args-gradle-plugin:2.8.5")
     }
 }
